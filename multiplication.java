24e26fb5dbea2dc9be607c3a8ce05a724ba3999b
Neeraj Ahire <64035068+nir7777@users.noreply.github.com>
2022-03-23 18:47:57 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,11 @@
+
+public class multiplication {
+	public static void main(String[] args)
+	{
+		int num = 9;
+		for(int i = 1; i <= 10; ++i)
+		{
+			System.out.printf("%d * %d = %d \n", num,i,num*i);
+		}
+	}
+}
